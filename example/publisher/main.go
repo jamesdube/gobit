7014e1b6f76658dbe419e665456e84b612efc9e8
James Dube <jamesdylandube@gmail.com>
2024-01-08 13:02:15 +0200

    Feature/upgrade (#3)
    cleanup and conflicts fix

--- example/publisher/main.go
+++ example/publisher/main.go
@@ -1,1 +1,31 @@
-package publisher
+package main
+
+import (
+	"fmt"
+	"github.com/jamesdube/gobit/lib/event"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log/slog"
+//	"time"
+)
+
+func main() {
+	logger := slog.Default()
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672")
+	if err != nil {
+		panic(err)
+	}
+
+	emitter, err := event.NewEventEmitter(conn)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 1; i < 100; i++ {
+		err := emitter.Publish("sms", "sms.econet", fmt.Sprintf("message number [%d]", i))
+		if err != nil {
+			return
+		}
+		logger.Info("published message", "id", i)
+//		time.Sleep(100 * time.Millisecond)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
 module github.com/jamesdube/gobit
 
-go 1.14
+go 1.21
 
-require github.com/streadway/amqp v1.0.0
+require (
+	github.com/google/uuid v1.5.0
+	github.com/rabbitmq/amqp091-go v1.9.0
+)
